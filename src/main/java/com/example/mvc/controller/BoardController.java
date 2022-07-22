8f64d06434b5c0193e601632fb8207f8437d8e9a
jhyunlee94 <jkwhd12@gmail.com>
2022-07-22 15:31:10 +0900

    0722 Thymeleaf, Validation

--- src/main/java/com/example/mvc/controller/BoardController.java
+++ src/main/java/com/example/mvc/controller/BoardController.java
@@ -2,13 +2,15 @@ package com.example.mvc.controller;
 
 import com.example.mvc.entity.Board;
 import com.example.mvc.repository.BoardRepository;
+import com.example.mvc.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -18,10 +20,37 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private BoardValidator boardValidator;
+
     @GetMapping("/list")
     public String list(Model model) {
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String form(Model model, @RequestParam(required = false) Long id) {
+        if(id == null) {
+            model.addAttribute("board", new Board());
+        }else {
+            //조회된 값이 없을 수 있어서 null로 처리
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+        }
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String form(@Valid Board board, BindingResult bindingResult){
+        if(bindingResult.hasErrors()) {
+            //값이 부합하는지 안하는지 hasErrors()로 처리가능
+            //GetMapping 에서 처리한거 넣어줌
+            return "board/form";
+        }
+        //다시 board를 받아오는거
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
---
+++ src/main/java/com/example/mvc/validator/BoardValidator.java
@@ -0,0 +1,28 @@
+package com.example.mvc.validator;
+
+import com.example.mvc.entity.Board;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+@Component
+public class BoardValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Board.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object obj, Errors e) {
+        Board b = (Board) obj;
+        if(ObjectUtils.isEmpty(b.getContent())) {
+            //StringUtils Board에 Content가 비어있는지 확인해보겠습니다.
+            //isEmpty가 사라져서 대체로
+            //ObjectUtils.isEmpty
+            e.rejectValue("content", "key","내용을 입력하세요");
+        }
+    }
+}
